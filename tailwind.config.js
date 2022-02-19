efaa5b68a4faa46cd9553a4530b81f203d29f700
Michael Berezovsky <mishka.ber@gmail.com>
2022-02-19 23:26:10 +0200

    Update to Tailwindcss 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
